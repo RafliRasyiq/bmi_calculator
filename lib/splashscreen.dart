70685113427d7dcb5f711038c1d810ca9cac5484
RafliRasyiq <68888303+RafliRasyiq@users.noreply.github.com>
2021-09-07 14:59:06 +0700

    updated

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -16,7 +16,7 @@ class _SplashScreen extends State<SplashScreen> {
   splashscreenStart() async {
     var duration = const Duration(seconds: 3);
     return Timer(duration, () {
-      Navigator.push(
+      Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => HomeScreen()),
       );
